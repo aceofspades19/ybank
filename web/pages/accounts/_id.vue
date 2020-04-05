14671560d5df8b2f97dd33e7b0890d50cc54df14
Jordan Evans <jordanevans19@gmail.com>
2020-04-04 23:41:52 -0700

    small changes to get it working

--- web/pages/accounts/_id.vue
+++ web/pages/accounts/_id.vue
@@ -97,10 +97,10 @@ export default {
     const that = this;
 
     axios
-      .get(`http://localhost:8000/api/accounts/${this.$route.params.id}`)
+      .get(`http://herpderp.ca/api/accounts/${this.$route.params.id}`)
       .then(function(response) {
         if (!response.data.length) {
-          window.location = "/";
+          window.location.href = "/";
         } else {
           that.account = response.data[0];
 
@@ -112,7 +112,7 @@ export default {
 
     axios
       .get(
-        `http://localhost:8000/api/accounts/${
+        `http://herpderp.ca/api/accounts/${
           that.$route.params.id
         }/transactions`
       )
@@ -147,7 +147,7 @@ export default {
       evt.preventDefault();
 
       axios.post(
-        `http://localhost:8000/api/accounts/${
+        `http://herpderp.ca/api/accounts/${
           this.$route.params.id
         }/transactions`,
 
@@ -160,10 +160,10 @@ export default {
       // update items
       setTimeout(() => {
         axios
-          .get(`http://localhost:8000/api/accounts/${this.$route.params.id}`)
+          .get(`http://herpderp.ca/api/accounts/${this.$route.params.id}`)
           .then(function(response) {
             if (!response.data.length) {
-              window.location = "/";
+              window.location.href = "/";
             } else {
               that.account = response.data[0];
             }
@@ -171,7 +171,7 @@ export default {
 
         axios
           .get(
-            `http://localhost:8000/api/accounts/${
+            `http://herpderp.ca/api/accounts/${
               that.$route.params.id
             }/transactions`
           )
